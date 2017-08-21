11d3e9433572d88197e5a274db2b0a7830f24121
Benoît Knecht <benoit.knecht@fsfe.org>
2017-08-21 09:19:57 +0200

    record: remove check for root CNAME
    A root CNAME is allowed in Cloudflare, where it's turned into a
    corresponding A record. Don't reject it during validation.

--- tests/test_octodns_record.py
+++ tests/test_octodns_record.py
@@ -869,15 +869,6 @@ class TestRecordValidation(TestCase):
             'value': 'foo.bar.com.',
         })
 
-        # root cname is a no-no
-        with self.assertRaises(ValidationError) as ctx:
-            Record.new(self.zone, '', {
-                'type': 'CNAME',
-                'ttl': 600,
-                'value': 'foo.bar.com.',
-            })
-        self.assertEquals(['root CNAME not allowed'], ctx.exception.reasons)
-
         # missing trailing .
         with self.assertRaises(ValidationError) as ctx:
             Record.new(self.zone, 'www', {
